e1e83d62ae8fedf4a5c463dde1b4e0c60c7a4c8f
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2022-02-21 14:31:06 +0100

    Move print functionality to extra function

--- src/lib.rs
+++ src/lib.rs
@@ -27,13 +27,10 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
         Err(TagError::ParseError) => panic!("Tag found but could not be parsed"),
         Err(e) => panic!("Some other error: {:?}",e),  // supress other warnings for the moment
     }
-    match f_tags.id3v1 {
-        Some(x) => println!("{}", x),
-        None => (),
-    }
-
+    f_tags.print_tags();
     Ok(())
 }
+
 
 pub fn read_bytewise_from_file(filename: &str) -> Result<Vec<u8>, io::Error> {
     let file = File::open(&filename)?;
@@ -48,8 +45,14 @@ pub fn read_bytewise_from_file(filename: &str) -> Result<Vec<u8>, io::Error> {
 
 pub fn extract_id3(data: &[u8], f_tags: &mut FileTags) -> Result<(), TagError> {
     let mut id3v1_tags = ID3v1::create_from_binary(&data);
+    let mut id3v2_tags = ID3v2::create_from_binary(&data);
     match id3v1_tags {
         Ok(x) => {f_tags.id3v1 = Some(x);} ,
+        Err(TagError::TagsNotFoundError) => (),  // ignore if not found
+        Err(e) => return Err(e) //propagate any other error
+    }
+    match id3v2_tags {
+        Ok(x) => {f_tags.id3v2 = Some(x);} ,
         Err(TagError::TagsNotFoundError) => (),  // ignore if not found
         Err(e) => return Err(e) //propagate any other error
     }
@@ -75,14 +78,28 @@ impl Config {
 // struct that collects all Tag variants for a File
 pub struct FileTags {
     id3v1: Option<ID3v1>,
-    //id3v2: Optional<ID3v2>,
+    id3v2: Option<ID3v2>,
 }
 
 impl FileTags {
     pub fn new() -> FileTags {
-        FileTags{ id3v1: None }
+        FileTags{
+            id3v1: None,
+            id3v2: None,
+        }
+    }
+    pub fn print_tags(self) {
+        match self.id3v1 {
+            Some(x) => println!("id3v1 found:\n{}", x),
+            None => println!("id3v1 not found\n"),
+        }
+        match self.id3v2 {
+            Some(x) => println!("id3v2 found:\n{}", x),
+            None => println!("id3v2 not found\n"),
+        }
     }
 }
+
 
 pub struct ID3v1 {
     title: String,
@@ -169,6 +186,55 @@ impl fmt::Display for ID3v1 {
 }
 
 
+pub struct ID3v2 {
+    id3_version: u8,  // not really important currently
+    id3_revision: u8,
+    unsynchronization: bool,
+    extended_header: bool,
+    experimental_indicator: bool,
+    //title: String,
+    //artist: String,
+    //album: String,
+    //year: u64,
+    //comment: String,
+    //track: Option<u8>,
+    //genre: u8,
+}
+
+impl ID3v2 {
+    pub fn create_from_binary(file_data: &[u8]) -> Result<ID3v2, TagError> {
+        // the tag structure is more complicated than in the v1 case
+        // check if the file starts with ID3
+        match &file_data[..3] {
+            b"ID3" => {  // tag was found
+                // slice the relevant part
+                let header = &file_data[..10];
+                let id3_version = &header[3];  // not really important currently
+                let id3_revision = &header[4];
+                let flags = BitArray::create_from_byte(header[5], true);
+                let unsynchronization = flags.bits[0];
+                let extended_header = flags.bits[1];
+                let experimental_indicator = flags.bits[2];
+                //{
+                    //Ok(x) => x,
+                    //Err(x) => return Err(TagError::ParseError),
+                //}
+
+                Ok(ID3v2 { id3_version: *id3_version, id3_revision: *id3_revision, unsynchronization, extended_header, experimental_indicator })
+                //Ok(ID3v2 { title, artist, album, year, comment, track, genre: *genre})
+            }
+            _ => return Err(TagError::TagsNotFoundError)
+        }
+    }
+}
+
+impl fmt::Display for ID3v2 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "id3_version: {}\nid3_revision: {}\nunsynchronization: {}\nextended_header: {}\nexperimental_indicator: {}\n",self.id3_version, self.id3_revision,self.unsynchronization,self.extended_header,self.experimental_indicator)
+    }
+
+}
+
 
 fn unsafe_u8_to_str(u8data: &[u8]) -> &str {
     str::from_utf8(&u8data).unwrap()
@@ -180,13 +246,9 @@ pub struct BitArray {
 }
 
 impl BitArray {
-    pub fn create_from_byte(byte: u8, big_endian: bool) -> Result<BitArray, ()> {
+    pub fn create_from_byte(byte: u8, big_endian: bool) -> BitArray {
         let mut tmp_byte = byte;
         let mut tmp_arr = [false; 8];  // initialize to 0
-        //match big_endian {
-            //true => tmp_byte = byte.to_be(),
-            //false => tmp_byte = byte.to_le(),
-        //}
         for i in 0..8 {  // loop over bits
             let last_bit = (tmp_byte % 2) == 1;  // set to true or false
             match big_endian {
@@ -196,7 +258,7 @@ impl BitArray {
             tmp_byte = tmp_byte >> 1;  // shift to next byte
 
         }
-        Ok(BitArray { bits: tmp_arr, big_endian } )
+        BitArray { bits: tmp_arr, big_endian }
 
     }
 }
@@ -214,7 +276,7 @@ mod tests {
     #[test]
     fn test_to_bitarray() {
         let byte = 5u8;
-        let bitarr = BitArray::create_from_byte(byte, true).unwrap();
+        let bitarr = BitArray::create_from_byte(byte, true);
         let result = [false,false,false,false,false,true,false,true]; // 5 in bits, big endian
         assert_eq!(bitarr.bits, result);
     }
